779a32643209ec1cf03779bdf7689b213bc95d40
Jobel Racines <jobelracines@gmail.com>
2019-09-23 15:03:34 +0800

    Update React Class to Hook

--- client/src/Components/Login/index.tsx
+++ client/src/Components/Login/index.tsx
@@ -6,127 +6,28 @@ import {Requests,Token} from 'Services';
 import { Redirect } from 'react-router';
 import jwt_decode from 'jwt-decode';
 
-class Login extends React.Component<any,any>{
+const logo = process.env.REACT_APP_LOGO;
 
-    constructor(props:any){
-        super(props);
 
-        this.state = {
-            redirect        : '',
-            submit          : false,
-            credentials     : {
-                username    :   '',
-                password    :   ''
-            },
-            error           : {
-                status      : false,
-                message     : '',
-            }
-        }
+const Login = (props:any) => {
+    
+    const [ redirect, setRedirect ] = React.useState('');
+    const [ submit, setSubmit ] = React.useState(false);
+    const [ credentials, setCredentials ] = React.useState({username : '',password : '',});
+    const [ error, setError ] = React.useState({status   : false,message  : ''});
 
-        this.submit = this.submit.bind(this);
-        this.update_input_text = this.update_input_text.bind(this);
-        this.checkUser = this.checkUser.bind(this);
-
-    }
-
-    render(){
-        if(this.state.redirect!== ''){
-            return <Redirect to={{pathname:this.state.redirect}}/>;
-        }
-
-        return(
-            <Container maxWidth="lg">
-                <div className="logo-login">
-                    <img src='/img/apsoft-logo.png' alt="logo" />
-                    <div className="logo-title">
-                        asset management system     
-                    </div>
-                </div>
-                <Card className="card-login">
-                    <CardHeader
-                        className = "header-login"
-                        avatar = {
-                            <div className="wrap-user-icon">
-                                <Avatar className="user-icon-login">
-                                    <AccountCircle className="user-icon-login1"/>
-                                </Avatar>
-                            </div>
-                        }
-                    />
-                    
-                    <CardContent className="card-content-login">
-                        <form onSubmit={this.submit}>
-                            <TextField
-                                className="username-login"
-                                label="Username"
-                                type="text"
-                                name="username"
-                                autoComplete="username"
-                                margin="normal"
-                                variant="outlined"
-                                disabled={this.state.submit}
-                                onChange={this.update_input_text}
-                                value={this.state.credentials.username}
-                                error = {this.state.error.status}
-                                required
-                            />
-
-                            <TextField
-                                className="password-login"
-                                label="Password"
-                                type="password"
-                                name="password"
-                                margin="normal"
-                                variant="outlined"
-                                disabled={this.state.submit}
-                                onChange={this.update_input_text}
-                                value={this.state.credentials.password}
-                                error = {this.state.error.status}
-                                helperText = { this.state.error.status ? <b dangerouslySetInnerHTML={{__html:this.state.error.message}}></b> : '' }
-                                required
-                            />
-                            <div className="submit-login">
-                                <Button variant="contained" color="primary" disabled={this.state.submit} type="submit">
-                                    Submit
-                                    { 
-                                        this.state.submit 
-                                        ? <CircularProgress />
-                                        : ''
-                                    }
-                                </Button>
-                            </div>
-                        </form>
-                    </CardContent>
-                </Card>
-            </Container>
-        );
-    }
-
-    update_input_text(event:any){
-        const a = this.state.credentials;
-
-        a[event.target.name] = event.target.value;
-
-        this.setState({
-            credentials : a
+    const update_input_text = (event:any) => {
+        setCredentials({
+            ...credentials,
+            [event.target.name] : event.target.value,
         });
     }
 
-    async submit(event:any){
+    const submitForm = async (event:any) => {
     
         event.preventDefault();
-
-        this.setState({
-            submit : true,
-            error: {
-                status : true,
-                message : '',
-            }
-        });
-
-        const credentials = this.state.credentials;
-
+        setSubmit(true);
+        setError({status:true,message:''});
 
         const a:any = await Requests.Auth.login({
             username : credentials.username,
@@ -135,17 +36,17 @@ class Login extends React.Component<any,any>{
 
         if(!a.network_error){
             switch(a.status){
-                case 401 : 
-                    credentials['password'] = "";
-
-                    this.setState({
-                        submit : false,
-                        error : {
-                            status : true,
-                            message : "Credentials does not exist!!"
-                        },
-                        credentials : credentials,
+                case 401 :
+                    setCredentials({
+                        ...credentials,
+                        password : "",
                     })
+                    setError({
+                        status : true,
+                        message: "Credentials does not exist!!",
+                    })
+                    setSubmit(false);
+
                     break;
                 case 200 :
 
@@ -153,25 +54,21 @@ class Login extends React.Component<any,any>{
 
                     const jwt:any = jwt_decode(Token.get()); // get id of current user using JWT payload
                     const user =await Requests.User.get(jwt.sub);
-                    this.checkUser(user);
+                    checkUser(user);
                     break;
-                default : 
-                    this.setState({
-                        submit : false,
-                        error : {
-                            status : true,
-                            message : "Something wrong with the server, please try again later!!!!"
-                        },
+                default :
+                    setSubmit(false);
+                    setError({
+                        status : true,
+                        message:  "Something wrong with the server, please try again later!!!!",
                     })
                     break;
             }
         }else{
-            this.setState({
-                submit : false,
-                error : {
-                    status : true,
-                    message : "Something wrong with the server. <br /> Please contact Administrator!!!!"
-                },
+            setSubmit(false);
+            setError({
+                status : true,
+                message : "Something wrong with the server. <br /> Please contact Administrator!!!!"
             })
         }
 
@@ -179,49 +76,108 @@ class Login extends React.Component<any,any>{
     }
 
 
-    checkUser(user:Requests.Format):any{
+    const checkUser = (user:Requests.Format):any => {
 
         if(user.status === 200){
             if(!user.data.activated){
-                this.setState({
-                    submit : false,
-                    error : {
-                        status : true,
-                        message : "The account is not authorized to log in. <br /> Please contact Administator"
-                    },
+                setSubmit(false);
+                setError({
+                    status : true,
+                    message: "The account is not authorized to log in. <br /> Please contact Administator"
                 });
 
                 Token.remove();
                 return;
             }
 
-            const returnUrl = this.props.location.state;
+            const returnUrl = props.location.state;
             
-            if(returnUrl === undefined || returnUrl === null ){
-                this.setState({
-                    redirect : '/',
-                })
+            if(!returnUrl){
+                setRedirect('/');
                 return;
             }
-            
-            this.setState({
-                redirect : returnUrl.from, 
-            })
+            setRedirect(returnUrl.from);
             return;
         }
-
-        this.setState({
-            submit : false,
-            error : {
-                status : true,
-                message : "Something went wrong.!!!<br /> Please contact Administator!!!"
-            },
-        });
-
+        setSubmit(false);
+        setError({
+            status : true,
+            message : "Something went wrong.!!!<br /> Please contact Administator!!!"
+        })
         Token.remove();
         return;
-
     }
+
+    if(redirect!== ''){
+        return <Redirect to={{pathname:redirect}}/>;
+    }
+
+    return(
+        <Container maxWidth="lg">
+            <div className="logo-login">
+                <img src={logo} alt="logo" />
+                <div className="logo-title">
+                    asset management system     
+                </div>
+            </div>
+            <Card className="card-login">
+                <CardHeader
+                    className = "header-login"
+                    avatar = {
+                        <div className="wrap-user-icon">
+                            <Avatar className="user-icon-login">
+                                <AccountCircle className="user-icon-login1"/>
+                            </Avatar>
+                        </div>
+                    }
+                />
+                
+                <CardContent className="card-content-login">
+                    <form onSubmit={submitForm}>
+                        <TextField
+                            className="username-login"
+                            label="Username"
+                            type="text"
+                            name="username"
+                            autoComplete="username"
+                            margin="normal"
+                            variant="outlined"
+                            disabled={submit}
+                            onChange={update_input_text}
+                            value={credentials.username}
+                            error = {error.status}
+                            required
+                        />
+
+                        <TextField
+                            className="password-login"
+                            label="Password"
+                            type="password"
+                            name="password"
+                            margin="normal"
+                            variant="outlined"
+                            disabled={submit}
+                            onChange={update_input_text}
+                            value={credentials.password}
+                            error = {error.status}
+                            helperText = {error.status ? <b dangerouslySetInnerHTML={{__html:error.message}}></b> : '' }
+                            required
+                        />
+                        <div className="submit-login">
+                            <Button variant="contained" color="primary" disabled={submit} type="submit">
+                                Submit
+                                { 
+                                    submit 
+                                    ? <CircularProgress />
+                                    : ''
+                                }
+                            </Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </Container>
+    );
 
 }
 
